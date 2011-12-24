1a47a038acec1a9c511302fd70ded569f252e51b
Michael Davis <m.r.davis@me.com>
2011-12-24 11:51:09 +0000

    Add Plone Keyword as a buildout dependency, otherwise upgrade fails

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name='younglives.content',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
+          'Products.PloneKeywordManager',
       ],
       extras_require = {
             'test': [
