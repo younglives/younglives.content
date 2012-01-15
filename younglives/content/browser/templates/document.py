e1eb34544d5212d375d2f520e7bd66d6137609a4
Michael Davis <m.r.davis@me.com>
2012-01-15 15:09:48 +0000

    The document view was customised to add the banner

---
+++ younglives/content/browser/templates/document.py
@@ -0,0 +1,26 @@
+""" Default browser view for Document items. """
+
+
+# Zope
+from Acquisition import aq_parent
+
+# Plone
+from plone.memoize import view
+from Products.Five import BrowserView
+from Products.CMFCore.utils import getToolByName
+
+
+class DocumentView(BrowserView):
+    
+    @view.memoize
+    def banner(self):
+        ptool = getToolByName(self.context, 'plone_utils')
+        parent = getattr(self.context, '__parent__', aq_parent(self.context)) 
+        if ptool.isDefaultPage(self.context):
+            try:
+                banner_field = parent.getField("bannerImage")
+            except AttributeError:
+                return None
+            if banner_field:
+                return banner_field.get(parent)
+        return None
