f49cc15ed5c330f57967f7dade63222e2daf97fa
Michael Davis <m.r.davis@me.com>
2012-08-16 19:59:22 +0000

    Remove the capture popup methods

--- younglives/content/browser/publication.py
+++ younglives/content/browser/publication.py
@@ -36,14 +36,13 @@ class PublicationView(BrowserView):
         files = self.context.getFiles()
         ltool = getToolByName(self.context, 'portal_languages')
         for file in files:
-            css = self._show_capture_popup() and 'capturePopup'
             lang = file.Language() or 'en'
             type_field = file.getField('documentType')
             type = type_field and type_field.get(file)
             results.append(dict(title = file.Title(),
                                 size = '%s KB' % (file.get_size() / 1024),
-                                url = self._get_file_url(file),
-                                css = css or '',
+                                url = file.absolute_url(),
+                                css = '',
                                 type = (type and type[0]) or 'file',
                                 language = ltool.getNameForLanguageCode(lang),
                                 content_type = self._get_file_mimetype(file)))
@@ -60,20 +59,3 @@ class PublicationView(BrowserView):
                     mimetype = MIME_TYPE['title']
                     break
         return mimetype
-
-
-    def _get_file_url(self, file):
-        if self._show_capture_popup():
-            portal= getMultiAdapter((self.context, self.request), 
-                                    name=u'plone_portal_state')
-            return "%s/capturePopup?uid=%s" % (portal.portal_url(), 
-                                               self.context.UID())
-        return file.absolute_url()
-    
-    
-    def _show_capture_popup(self):
-        pm = getToolByName(self.context, 'portal_membership')
-        if pm.isAnonymousUser() and \
-            not self.request.cookies.get(CAPTURE_DATA_COOKIE_NAME):
-            return 1
-        return 0
