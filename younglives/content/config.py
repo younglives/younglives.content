052ddee84164f5744808d22e2d592b67d3d22612
Michael Davis <m.r.davis@me.com>
2015-08-01 12:25:46 +0100

    More unused code

--- younglives/content/config.py
+++ younglives/content/config.py
@@ -1,7 +1,3 @@
 PROJECTNAME = 'younglives.content'
 
-ADD_PERMISSIONS = {
-    'Publication': 'younglives.content: Add Publication',
-}
-
-CAPTURE_DATA_COOKIE_NAME = 'captured'
+ADD_PERMISSIONS = {}
